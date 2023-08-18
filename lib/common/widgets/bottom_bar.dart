130294dc72c601386db7f505881cbf64ed063ab2
Rishit158 <132547382+Rishit158@users.noreply.github.com>
2023-08-18 18:51:44 +0530

    Initial commit

---
+++ lib/common/widgets/bottom_bar.dart
@@ -0,0 +1,44 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class BottonBar extends StatefulWidget {
+  static const String routeName = '/actual-home';
+  const BottonBar({super.key});
+
+  @override
+  State<BottonBar> createState() => _BottonBarState();
+}
+
+class _BottonBarState extends State<BottonBar> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: GlobalVariables.selectedNavBarColor,
+        unselectedItemColor: GlobalVariables.unselectedNavBarColor,
+        backgroundColor: GlobalVariables.backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 0
+                        ? GlobalVariables.selectedNavBarColor
+                        : GlobalVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
